ade1f2680b4c9181fddd00cf0d74a582cd792d99
igorhristov <igorhristov@sourcemxacademy.mk>
2020-06-28 12:14:26 +0200

    first commit

---
+++ src/build/precache-manifest.73ed8e2caf9bccbb1f9c95fda838faa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60116f5f290efd33aab75b359489997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c943f83fd81d6f4cf38",
+    "url": "/static/css/main.82457fc8.chunk.css"
+  },
+  {
+    "revision": "ae862ab7ddfa6ad6ffca",
+    "url": "/static/js/2.1cbf39ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1cbf39ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c943f83fd81d6f4cf38",
+    "url": "/static/js/main.90bec34f.chunk.js"
+  },
+  {
+    "revision": "8427caf53a3749d2d891",
+    "url": "/static/js/runtime-main.37f64c89.js"
+  }
+]);
---
+++ src/server.js
@@ -0,0 +1,86 @@
+import express from 'express';
+const mongoose = require('mongoose');
+import path from 'path';
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/build')));
+mongoose.set('useFindAndModify', false);
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new mongoose.Schema({
+    name: String,
+    upvotes: Number,
+    comments: Array,
+});
+const Articles = mongoose.model('Article', ArticleSchema);
+
+mongoose.connect(
+    'mongodb+srv://tester:P2cEHncejcpRIvlo@cluster0-te9ss.mongodb.net/articles?retryWrites=true&w=majority',
+    { useUnifiedTopology: true, useNewUrlParser: true },
+    () => console.log('Connected to database!')
+);
+
+app.get('/api/articles', async (req, res) => {
+    try {
+        const articles = await Articles.find({});
+        res.send(articles);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+app.get('/api/articles/:name', async (req, res) => {
+    try {
+        const article = await Articles.findOne({ name: req.params.name });
+
+        res.status(200).send(article);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    try {
+        const updateUpvote = await Articles.findOneAndUpdate(
+            { name: req.params.name },
+            {
+                $inc: { upvotes: 1 },
+            },
+            { new: true }
+        );
+        res.status(200).json(updateUpvote);
+    } catch (err) {
+        res.status(400);
+    }
+});
+
+app.post('/api/articles/:name/add-comment', async (req, res) => {
+    try {
+        const { username, text } = req.body;
+        const addComment = await Articles.findOneAndUpdate(
+            { name: req.params.name },
+            {
+                $push: { comments: { username, text } },
+            },
+            { new: true }
+        );
+        res.status(200).json(addComment);
+    } catch (err) {
+        res.status(400);
+    }
+
+    // const { username, text } = req.body;
+    // const articleName = req.params.name;
+
+    // articlesInfo[articleName].comments.push({ username, text });
+
+    // res.status(200).send(articlesInfo[articleName]);
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+app.listen(8000, () => console.log('Listening on port 8000'));
